aff705d423fdf67599e7b6259703b1d9cb0a9dda
animewoman <bekameilun@gmail.com>
2019-03-25 13:32:54 +0600

    expy13

---
+++ expy13.py
@@ -0,0 +1,9 @@
+#working with terminal input
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called", script)
+print("Your first var is", first)
+print("Your second var is", second)
+print("Your third var is", third)
