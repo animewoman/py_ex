c71959ef7cd15f25c410ca5da131fee422bde90a
animewoman <bekameilun@gmail.com>
2019-03-25 13:24:20 +0600

    expy10

---
+++ expy10.py
@@ -0,0 +1,10 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+
+"""
